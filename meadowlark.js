1961af2a1e2f39cd4eec3fd7ebeb00050ade487a
wilcook <wil.cook.is@gmail.com>
2017-03-11 20:30:51 +0000

    chapter 5

--- meadowlark.js
+++ meadowlark.js
@@ -15,12 +15,18 @@ app.set('port', process.env.PORT || 1312);
 
 app.use(express.static(__dirname + '/public'));
 
+app.use(function(req, res, next){
+	res.locals.showTests = app.get('env') !== 'production' && req.query.test ==='1';
+	next();
+})
+
 app.get('/', function(req, res){
 	res.render('home');
 });
 
 app.get('/about', function(req, res){
-	res.render('about', { fortune: fortune.getFortune() });
+	res.render('about', { fortune: fortune.getFortune(),
+						  pageTestScript: '/qa/tests-about.js' });
 });
 
 // custom 404 page
